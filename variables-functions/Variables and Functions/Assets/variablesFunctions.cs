7816bf730eb3a4bd16fdffc9212124d6b9d5ccde
Austin Bennett <Austin@Austins-MBP-2.ad.uvu.edu>
2016-01-07 17:59:31 -0700

    Initial commit

---
+++ variables-functions/Variables and Functions/Assets/variablesFunctions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class variablesFunctions : MonoBehaviour {
+
+	int myInt = 5;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(myInt * 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ variables-functions/variables-functions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+namespace variablesfunctions
+{
+	class MainClass
+	{
+		int myInt = 5;
+
+		void Start(){
+			Debug.Log (myInt * 2);
+		}
+	}
+}
