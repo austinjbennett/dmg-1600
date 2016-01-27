ba0b6854f67497796b1413c02e860719d2f1e2dd
Austin Bennett <Austin@Austins-MBP.ad.uvu.edu>
2016-01-26 18:18:45 -0700

    New Unity project for all future assignments

---
+++ Unity Files/Assets/ifStatements.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ifStatements : MonoBehaviour {
+
+	int maxNum = 6;
+	int currentNum = 0;
+
+	// Use this for initialization
+	void Start () {
+		if (currentNum == maxNum) {
+			Debug.Log('The current number is at the max!');
+		}
+		else if (currentNum < maxNum) {
+			Debug.Log('You\'re not quite at the max yet');
+		}
+		else {
+			Debug.Log('Whoa, you\'ve gone too far dude');
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
