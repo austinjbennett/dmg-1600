3cb8ff10d0cf75e1d1a72887a05734ad8bdc89f0
Austin Bennett <Austin@Austins-MBP-2.ad.uvu.edu>
2016-02-11 17:24:07 -0700

    If statements

--- Unity Files/Assets/ifStatements.cs
+++ Unity Files/Assets/ifStatements.cs
@@ -4,20 +4,18 @@ using System.Collections;
 public class ifStatements : MonoBehaviour {
 
 	int maxNum = 6;
-	int currentNum = 0;
+	int currentNum = 7;
 
 	// Use this for initialization
 	void Start () {
 		if (currentNum == maxNum) {
-			Debug.Log('The current number is at the max!');
+			Debug.Log ("You are at the max number");
+		} else if (currentNum < maxNum) {
+			Debug.Log ("You are not quite at the maximum yet");
+		} else {
+			Debug.Log ("You have surpassed the maximum value");
 		}
-		else if (currentNum < maxNum) {
-			Debug.Log('You\'re not quite at the max yet');
-		}
-		else {
-			Debug.Log('Whoa, you\'ve gone too far dude');
-		}
-	}
+	}	
 	
 	// Update is called once per frame
 	void Update () {
