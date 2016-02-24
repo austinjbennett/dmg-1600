95509a8706c3e921bd373283fceb5b4ee80b764c
Austin Bennett <Austin@Austins-MBP.ad.uvu.edu>
2016-02-23 17:55:35 -0700

    Adding the scope and access modifiers assignment

---
+++ Unity Files/Assets/scope2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scope2 : MonoBehaviour {
+
+	private scopeAndAccessModifiers myOtherClass;
+
+
+	// Use this for initialization
+	void Start () {
+		myOtherClass = new scopeAndAccessModifiers();
+		Debug.Log ("You have " + myOtherClass.numTwo + myOtherClass.apple + "Apples");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity Files/Assets/scopeAndAccessModifiers.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scopeAndAccessModifiers : MonoBehaviour {
+
+	public int myNum = 3;
+	public int numTwo = 23;
+	public string apple = " Granny Smith ";
+
+
+	// Use this for initialization
+	void Start () {
+		int numThree = 28;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
