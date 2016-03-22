c31801f0aabbf7d3f8d357613f1395fe4b6c3405
Austin Bennett <Austin@Austins-MBP.ad.uvu.edu>
2016-03-22 17:48:58 -0600

    Classes and Arrays

---
+++ Classes and Data Types/Assets/arrays.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arrays : MonoBehaviour {
+
+	public string[] cars = new string[3];
+	//string[] cars = { "bmw", "hyundai", "volkswagen" };
+
+
+	// Use this for initialization
+	void Start () {
+		cars [0] = "bmw";
+		cars [1] = "hyundai";
+		cars [2] = "volkswagen";
+
+		for (int i = 0; i < cars.Length; i++) {
+			Debug.Log (cars [i]);
+		}
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Classes and Data Types/Assets/classesAndDataTypes.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class classesAndDataTypes : MonoBehaviour {
+
+	public class Donut {
+
+		string icing;
+		string flavor;
+		bool sprinkles;
+
+		public Donut() {
+
+			icing = "chocolate";
+			flavor = "vanilla";
+			sprinkles = true;
+		}
+
+		public Donut(string ic, string fl, bool sp) {
+			icing = ic;
+			flavor = fl;
+			sprinkles = sp;
+		}
+
+	}
+
+	Donut chocolate = new Donut();
+	Donut maple = new Donut("maple", "vanilla", false);
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
