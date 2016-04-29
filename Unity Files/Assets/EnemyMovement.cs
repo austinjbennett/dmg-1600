8c45d71e865f64b092c4e611184f1acc583b90e4
Austin Bennett <Austin@Austins-MacBook-Pro-3.local>
2016-04-28 21:12:35 -0600

    Changes to Enemy Movement script

--- Unity Files/Assets/EnemyMovement.cs
+++ Unity Files/Assets/EnemyMovement.cs
@@ -2,35 +2,43 @@
 using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
-    /* 
-     * Variables:
-     * A private Rigidbody variable for the Rigidbody component
-     * A public float for the speed
-     * A private Vector3 for the direction
-     */
+
 	Rigidbody rigidbody;
 	public float speed;
 	Vector3 direction;
+	GameObject player;
+	EnemyDamage enemydamage;
 
-    /*
-     * The Start function, void return type, no parameters
-     *      Use GetComponent to get the rigidbody reference
-     *      Have the direction variable equal a new Vector3 with X and Z being zero and Y being a Random number between 0 and 360.
-     *      Have eulerangles equal the direction variable in order to rotate this enemy
+	/*
+     * Additional Lines in the Start Function:
+     *      Use FindGameObjectWithTag with the player's tag in order to get the reference to the player - place this line above the ones that were there before
+     *      Use GetComponent to get the reference to your enemy damage script - place this line above the ones that were there before
+     *      If (the type variable in the enemy damage script is equal to the first enum constant in the enemy damage script) - place this if statement at the bottom of the function
+     *          Move the line that sets the random direction of the enemy into this If statement
      */
+
 	void Start() {
+		enemydamage = GetComponent<EnemyDamage>();
 		rigidbody = GetComponent<Rigidbody> ();
-		direction = new Vector3(0, Random.Range(0, 360), 0);
 		transform.eulerAngles = direction;
 		rigidbody.AddRelativeForce (transform.forward * speed);
+		player = GameObject.FindGameObjectWithTag ("Player");
+		if (enemydamage.enemytype == EnemyDamage.EnemyType.weak) {
+			direction = new Vector3(0, Random.Range(0, 360), 0);
+		}
 	}
-	
-    /*
-     * The Update function, void return type, no parameters
-     *      Using the Rigidbody variable call AddRelativeForce and pass the forward direction times the speed as parameters
+
+	/*
+     * Additional Lines in the Update Function:
+     *      If (the type variable in the enemy damage script is equal to the second enum constant OR the third enum constant in the enemy damage script)
+     *          Use the LookAt function with the player's transform.
      */
+
 	void Update() {
 		rigidbody.AddRelativeForce (transform.forward * speed);
+		if (enemydamage.enemytype == EnemyDamage.EnemyType.strong || enemydamage.enemytype == EnemyDamage.EnemyType.superstrong) {
+			transform.LookAt(player.transform);
+		}
 	}
 
 
@@ -74,3 +82,55 @@ public class EnemyMovement : MonoBehaviour {
      *                    *rigidbodyVariable*.AddRelativeForce(transform.forward * speed);
      */
 }
+
+/* **************Explanation and Hints************ 
+     *  Damage Script variable type - When we want to use functions and variables from another script we need to have a variable that uses the script
+     *                                as the type. In this script we want to use the enemy damage script. So we would write it like this:
+     *                                
+     *                                private *enemyDamageScriptName* *enemyDamageVariableName*;
+     *                                
+     *                                Replace *enemyDamageScriptName* with the name of your enemy damage script. Replace *enemyDamageVariableName* with a name of 
+     *                                your choosing. Please look at the Classes and Datatypes assignment for more details.
+     *                                
+     * Accessing variables in the enemy damage script - To do this we need to use the dot operator. Please look at the Scope and Access Modifier assignment as well
+     *                                                  as the Classes and Datatypes assignment. 
+     * 
+     * FindGameObjectWithTag - This will find a GameObject with a specific tag. In this case we are using it to get a reference to the player.
+     *                         You can do this by typing out the following
+     *
+     *                         *nameOfPlayerVariable* = GameObject.FindGameObjectWithTag("EnterPlayerTagHere");
+     *                         
+     *  GetComponent - This allows us to get a reference to a script that's attached to the same GameObject that this script is attached to. In this
+     *                  script we want to get a reference to the enemy damage script. This can be done by typing the following:
+     *                  
+     *                  *enemyDamageVariableName* = GetComponent<*enemyDamageScriptName*>();
+     *                  
+     *                  Replace *enemyDamageScriptName* with the name of your enemy damage script. Replace *enemyDamageVariableName* with a name of 
+     *                  your choosing. 
+     *
+     * Getting a reference to the enemy damage script - You can do this the same way as getting the rigidbody except you replace "rigidbody" for
+     *                                                  the name of the script. For example:
+     *                                                   
+     *                                                  *enemyDamageVariableName* = GetComponent<*enemyDamageScriptName*>();
+     *
+     * Comparing the type of enemy - In order to figure out what type of enemy this is we need to use the enemy damage variable from above.
+     *                               Then we use the dot operator to access the type variable in that script. After the double equals we need
+     *                               to use the enemy damage variable once again to access the enum we are comparing the type to.
+     *
+     * LookAt - The LookAt function rotates this GameObject to face a target. In this case we want to rotate the enemy so that it is facing the player.
+     *          We can do this by typing the following:
+     *          
+     *          transform.LookAt(*nameOfPlayerVariable*.transform);
+     *
+     * Enemy type variable in the EnemyDamage script - This works similar to getting the x variable from Vector3. In our case the Enemy Type
+     *                                                variable is in the Enemy Damage script just as the x variable is in Vector3. So we 
+     *                                                will want to do the following:
+     *          
+     *                                                *enemyDamageVariable*.*enemyTypeVariable*
+     *
+     *                                                - To be able to compare the enemy type variable to one of the constants we will need to
+     *                                                  do something similar except instead of the enemy damage variable we will use the name
+     *                                                  of the class itself.
+     *
+     *                                                  *EnemyDamageScript*.*enumName*.*constant*
+     */
